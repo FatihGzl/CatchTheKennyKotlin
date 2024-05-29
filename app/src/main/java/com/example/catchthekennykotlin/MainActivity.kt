ad25d00902a3582a4209d26c4923f35f7babfe12
FatihGzl <f.al-guzel-51@hotmail.com>
2024-05-30 02:23:19 +0300

    CatchTheKennyKotlin committed.

---
+++ app/src/main/java/com/example/catchthekennykotlin/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.catchthekennykotlin
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.catchthekennykotlin.databinding.ActivityMainBinding
+import kotlin.random.Random as Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var score = 0
+    var imageArray = ArrayList<ImageView>()
+    var runnable = Runnable {}
+    var handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //ImageArray
+        imageArray.add(binding.imageView)
+        imageArray.add(binding.imageView2)
+        imageArray.add(binding.imageView3)
+        imageArray.add(binding.imageView4)
+        imageArray.add(binding.imageView5)
+        imageArray.add(binding.imageView6)
+        imageArray.add(binding.imageView7)
+        imageArray.add(binding.imageView8)
+        imageArray.add(binding.imageView9)
+
+        hideImages()
+
+        //CountDownTimer
+
+        object : CountDownTimer(15500, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.timeText.text = "Time : ${millisUntilFinished / 1000}"
+            }
+
+            override fun onFinish() {
+                binding.timeText.text = "Time : 0"
+                handler.removeCallbacks(runnable)
+
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+
+                //alert dialog
+
+                val alert = AlertDialog.Builder(this@MainActivity)
+                alert.setTitle("Game Over")
+                alert.setMessage("Restart The Game?")
+                alert.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
+                    //restart
+                    val intentFromMain = intent
+                    finish()
+                    startActivity(intentFromMain) //kendine intent yaptırdık, kendine intent yapıp bu aktiviteyi sıfırdan başlatacak
+                })
+
+                alert.setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this@MainActivity,"Game Over!",  Toast.LENGTH_LONG).show()
+                })
+
+                alert.show()
+            }
+        }.start()
+
+    }
+
+    fun hideImages() {
+
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+                val random = java.util.Random()
+                val randomIndex = random.nextInt(9)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 500)
+            }
+        }
+        handler.post(runnable)
+    }
+
+    fun increaseScore(view : View) {
+        score = score + 1
+        binding.scoreText.text = "Score : ${score}"
+    }
+
+}
